e8e2a70a66d73a2e3887d23a178e506c43b42e82
Kjetil Mehl <kjetil@mehl.no>
2021-02-17 22:34:37 +0100

    Generates typescript

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 yarn add @openapitools/openapi-generator-cli @nanobox/openapi-nano-spec --dev
 yarn openapi-generator-cli generate -g typescript-node -i ./node_modules/@nanobox/openapi-nano-spec/spec/openapi.yaml -o . -c config-typescript.json
+yarn build
