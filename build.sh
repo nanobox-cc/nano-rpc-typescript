5bd8a1dccb15496de6d3f2b77ca1768bd7f11887
Kjetil Mehl <kjetil@mehl.no>
2021-02-17 22:24:18 +0100

    Initial commit of generated files

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yarn add @openapitools/openapi-generator-cli @nanobox/openapi-nano-spec --dev
-yarn openapi-generator-cli generate -g typescript-node -i ./node_modules/@nanobox/openapi-nano-spec/spec/openapi.yaml -o ./clients/typescript -c config-typescript.json
+yarn openapi-generator-cli generate -g typescript-node -i ./node_modules/@nanobox/openapi-nano-spec/spec/openapi.yaml -o . -c config-typescript.json
